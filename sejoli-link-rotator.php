ca327fff84b779a4483136280d30599de98532c2
Henri Susanto <henry.dinus@gmail.com>
2023-10-12 19:43:34 +0700

    init plugin

---
+++ sejoli-link-rotator.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Sejoli Link Rotator
+ *
+ * @package     SejoliLinkRotator
+ * @author      Henri Susanto
+ * @copyright   2022 Henri Susanto
+ * @license     GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name: Sejoli Link Rotator
+ * Plugin URI:  https://github.com/susantohenri/sejoli-link-rotator
+ * Description: WordPress Plugin to Rotate Affiliate Link of Sejoli
+ * Version:     1.0.0
+ * Author:      Henri Susanto
+ * Author URI:  https://github.com/susantohenri/
+ * Text Domain: SejoliLinkRotator
+ * License:     GPL v2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
